548363681af5dadb8416cb2fab4d4028532036c5
mieyebi <96272004+mieyebi@users.noreply.github.com>
2022-06-01 22:02:54 +0000

    add and pow

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -7,12 +7,12 @@ elif (number >= 10 or number < 100) or (number <= -10 or number > -100):
     last_digit = number % 10
 elif (number >= 100 or number < 1000) or (number <= -100 or number > -1000):
     last_digit = number % 100
-elif (number >= 1000 and number < 10000) or (number <= -1000 and number > -10000):
+elif (number>=1000 and number<10000) or (number<=-1000 and number>-10000):
     last_digit = number % 1000
 
 if last_digit > 5:
-    print("Last digit of {} is {} and is greater than 5".format(number, last_digit))
+    print(f"Last digit of {number} is {last_digit} and is greater than 5")
 elif last_digit == 0:
     print("Last digit of {} is {} and is 0".format(number, last_digit))
 elif last_digit < 6 and last_digit != 0:
-    print("Last digit of {} is {} and is less than 6 and not 0".format(number, last_digit))
+    print(f"Last digit of {number} is {last_digit} and is less than 6 and not 0")
