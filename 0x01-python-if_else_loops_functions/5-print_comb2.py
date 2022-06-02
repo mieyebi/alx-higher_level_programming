ab293a027b0ed8c6c1293ec8f5d085495d20e5d8
mieyebi <96272004+mieyebi@users.noreply.github.com>
2022-06-02 00:03:20 +0000

    print coomb

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 for i in range(0, 100):
-    if i < 10:
+    if i < 99:
         print("{:0>2d}, ".format(i), end = "")
-    elif i == 99:
-        print("{:>2}".format(i))
     else:
         print("{:>2}, ".format(i), end="")
