990fe7a87afa37fb7dba92a3b9607bd25fe2fe72
mieyebi <96272004+mieyebi@users.noreply.github.com>
2022-06-02 23:42:41 +0000

    hidden discovery

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,4 +3,4 @@ if __name__ =="__main__":
     import hidden_4
     for a in dir(hidden_4):
         if (a[0] and a[1]) != '_':
-            print("{}".format(ord(a)))
+            print("{}".format(a))
