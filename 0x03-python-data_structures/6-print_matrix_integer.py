471c670d8051bddf83e0bdd7e9ac0b94ff7b81ec
mieyebi <96272004+mieyebi@users.noreply.github.com>
2022-06-06 20:08:41 +0000

    add tuples

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def print_matrix_integer(matrix=[[]]):
+    a = 0
+    for x in matrix:
+        for y in x:
+            print("{}".format(y), end="")
+
---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+
+def add_tuple(tuple_a=(), tuple_b=()):
+
+    a = len(tuple_a) - 1
+    b = len(tuple_b) - 1
+
+    if a < 0:
+        tuple_a = 0, 0
+    elif a == 0:
+       tuple_a = tuple_a[0], 0
+    elif a >= 1:
+        tuple_a = tuple_a[0], tuple_a[1]
+    if b < 0:
+        tuple_b = 0, 0
+    elif b == 0:
+        tuple_b = tuple_b[0], 0
+    elif b >= 1:
+       tuple_b = tuple_b[0], tuple_b[1]
+
+    x = tuple_a[0] + tuple_b[0]
+    y = tuple_a[1] + tuple_b[1]
+
+    return (x, y)
