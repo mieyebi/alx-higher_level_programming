152fb4dafe44335e1c0eaffdc56d2f3702c30de4
mieyebi <96272004+mieyebi@users.noreply.github.com>
2022-06-07 07:06:58 +0000

    new list divisible by two

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -2,3 +2,11 @@
 
 def divisible_by_2(my_list=[]):
 
+    new_list = []
+    for a in my_list:
+        if a % 2 == 0:
+            new_list.append(True)
+        elif a % 2 == 1:
+            new_list.append(False)
+
+    return new_list
