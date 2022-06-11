3f8a340a1553e5e3105c2197c11b841aab6e2309
mieyebi <96272004+mieyebi@users.noreply.github.com>
2022-06-11 00:56:07 +0000

    advanced tasks

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def complex_delete(a_dictionary, value):
+    dkeys = list(a_dictionary.keys())
+
+    for a in dkeys:
+        if val == a_dictionary.get(a):
+            del a_dictionary[a]
+
+    return (a_dictionary)
