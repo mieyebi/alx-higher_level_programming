e10250e2140c06d0a6138454c5c22f88ca30d72d
mieyebi <96272004+mieyebi@users.noreply.github.com>
2022-06-11 00:47:54 +0000

    best keys

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,12 +2,7 @@
 
 def best_score(a_dictionary):
 
-    dlist = []
-    if a_dictionary:
-        for a in a_dictionary.items():
-            dlist.append(item[1])
-        dlist = sorted(dlsit, reverse=True)
-        best = dlist[0]
-        for key, best_val in a_dictionary.items():
-            if best_val == best:
-                return key
+   if not a_dictionary:
+        return (None)
+
+    return (max(a_dictionary, key=a_dictionary.get))
--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
 
-    new_dir = a_dictionary.copy()
-    dkeys = list(new_dir.keys())
-
-    for a in list_keys:
-        new_dir[a] *= 2
-
-    return (new_dir)
+    dict = {}
+    for a, b in a_dictionary.items():
+        dict[a] = b*2
+    return (dict)
