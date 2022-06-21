08393b5a5840dbe6e25707f466b3a6a4128f06dc
mieyebi <96272004+mieyebi@users.noreply.github.com>
2022-06-21 03:01:03 +0000

    print list

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -10,6 +10,6 @@ def safe_print_division(a, b):
     except TypeError:
         div = None
     finally:
-        print('Inside result: {}'.format(quotient))
+        print('Inside result: {}'.format(div))
 
     return div
