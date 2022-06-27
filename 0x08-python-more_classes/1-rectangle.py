c4fcaa5a803615c02d8549ef53f64b80e7c3af81
mieyebi <96272004+mieyebi@users.noreply.github.com>
2022-06-27 21:41:42 +0000

    set rectangle

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -9,31 +9,28 @@ class Rectangle:
         self.height = height
 
     @property
-        def width(self):
-            """sets the property of the width"""
-            return self.__width
+    def width(self):
+        """sets the property of the width"""
+        return self.__width
 
     @width.setter
-        def width(self, value):
-            """the width setter"""
-            if not isinstance(width, int):
-                raise TypeError("width must be an integer")
-            if width < 0:
-                raise ValueError("width must be >= 0")
-            self.__width = width
-
+    def width(self, value):
+        """the width setter"""
+        if not isinstance(width, int):
+            raise TypeError("width must be an integer")
+        if width < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = width
     @property
-        """setting the decorator property for height"""
-        def height(self):
-            """defines the height to be set"""
-            return self.__height
+    def height(self):
+        """defines the height to be set"""
+        return self.__height
 
     @height.setter
-        """sets the height"""
-        def height(self, height):
-            """defines the set height"""
-            if not isinstance(height, int):
-                raise TypeError("height must be an integer""")
-            if height < 0:
-                raise ValueError("height must be >= 0""")
-            self.__height = height
+    def height(self, height):
+        """defines the set height"""
+        if not isinstance(height, int):
+            raise TypeError("height must be an integer""")
+        if height < 0:
+            raise ValueError("height must be >= 0""")
+        self.__height = height
