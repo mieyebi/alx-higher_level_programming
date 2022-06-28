f1141a9e2e1a477db2a998f9b8c7de0c6da2604d
mieyebi <96272004+mieyebi@users.noreply.github.com>
2022-06-28 07:12:20 +0000

    str and repr

--- 0x08-python-more_classes/4-rectangle.py
+++ 0x08-python-more_classes/4-rectangle.py
@@ -67,4 +67,4 @@ class Rectangle:
 
         def __repr__(self):
             """represents the object"""
-            return "Rectangle(self.width + "," + self.height)
+            return "Rectangle({}, {})".format(self.width, self.height)
