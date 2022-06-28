b29938e2a15151e1a273f5f8c76d75337b9bed2f
mieyebi <96272004+mieyebi@users.noreply.github.com>
2022-06-28 11:01:45 +0000

    number of instances

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -4,7 +4,7 @@
 class Rectangle:
     """defines the class rectangle"""
 
-     number_of_instances = 0
+    number_of_instances = 0
 
     def __init__(self, width=0, height=0):
         """defines the rectangle attributes"""
