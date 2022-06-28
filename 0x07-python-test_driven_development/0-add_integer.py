a2f0377a81f3c70e7f136dad840d8b08709b9647
mieyebi <96272004+mieyebi@users.noreply.github.com>
2022-06-28 15:06:33 +0000

    add integers

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -2,21 +2,13 @@
 """a function that adds two integers"""
 
 
-import math
 def add_integer(a, b=98):
     """defines the function that adds integers"""
 
-    while a and b:
-        if a != int:
-            if a == float:
-                a = (int)a
-            else:
-                raise TypeError("a must be an integer")
-        if b != int:
-            if b == float:
-                b = (int)b
-            else:
-                raise TypeError("b must be an integer")
+    if a is not isinstance(a,int) and not isinstance(a, float):
+        raise TypeError("a must be an integer")
+    if b is not isinstance(a,int) and not isinstance(a, float):
+        raise TypeError("b must be an integer")
 
-        sum = a + b
+    sum = (int)a + (int)b
     return sum
