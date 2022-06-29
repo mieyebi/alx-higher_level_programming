465374e9d213950c839e5e7fe39c97fa8373aad0
mieyebi <96272004+mieyebi@users.noreply.github.com>
2022-06-29 21:37:17 +0000

    compare for equality

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -79,17 +79,17 @@ class Rectangle:
         """represents the object"""
         return "Rectangle({}, {})".format(self.width, self.height)
 
-     @staticmethod
-        def bigger_or_equal(rect_1, rect_2):
-            """compares rectangles for equality"""
-            if not isinstance(rect_1, Rectangle):
-                raise TypeError("rect_1 must be an instance of Rectangle")
-            if not isinstance(rect_2, Rectangle):
-                 raise TypeError("rect_2 must be an instance of Rectangle")
-            if rect_1.area() >= rect_2.area():
-                return rect_1
-            else:
-                return rect_2
+    @staticmethod
+    def bigger_or_equal(rect_1, rect_2):
+        """compares rectangles for equality"""
+        if not isinstance(rect_1, Rectangle):
+            raise TypeError("rect_1 must be an instance of Rectangle")
+        if not isinstance(rect_2, Rectangle):
+            raise TypeError("rect_2 must be an instance of Rectangle")
+        if rect_1.area() >= rect_2.area():
+            return rect_1
+        else:
+            return rect_2
 
     def __del__(self):
         """deletes rectangle"""
