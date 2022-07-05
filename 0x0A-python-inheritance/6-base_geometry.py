db7d0e415fafd4abd47e1db2c48d718dd6d12d05
mieyebi <96272004+mieyebi@users.noreply.github.com>
2022-07-05 02:18:13 +0000

    inherited class 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""This is a BaseGeometry class with a method that
+    raises an exception"""
+
+
+class BaseGeometry:
+    """A class with a public instance method that raises an exception"""
+
+    def area(self):
+        """this is a method that raises an exception"""
+        raise Exception("Area() is not implemented")
