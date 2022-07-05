1fde47d1ae5115c1caf4e52e80b6a400c25ea0d9
mieyebi <96272004+mieyebi@users.noreply.github.com>
2022-07-05 02:39:06 +0000

    class inheritance 7

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""This is a BaseGeometry class with a method that
+    raises an exception"""
+
+
+class BaseGeometry:
+    """A class with a public instance method that raises an exception"""
+
+    def __init__(self, name, value):
+        self.name = name
+        self.value = value
+
+    def area(self):
+        """this is a method that raises an exception"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """this is aa mathod that validates a value"""
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        elif value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
