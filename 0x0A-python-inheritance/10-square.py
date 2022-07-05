5ea93bbd3bf09f9bb40e67be472e1dd5723d31d0
mieyebi <96272004+mieyebi@users.noreply.github.com>
2022-07-05 03:25:54 +0000

    class inheritance 10, 11

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -3,7 +3,19 @@
 
 
 
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
+class BaseGeometry:
+    """A class with a public instance method that raises an exception"""
+
+    def area(self):
+        """this is a method that raises an exception"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """this is a mathod that validates a value"""
+        if type(value) is not int:
+            raise TypeError("{} must be an integer".format(name))
+        elif value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
 
 
 class Rectangle(BaseGeometry):
