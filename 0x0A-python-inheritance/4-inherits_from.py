3b3f31b4b6f5303a36a01b859fd952ada6d38493
mieyebi <96272004+mieyebi@users.noreply.github.com>
2022-07-05 02:23:52 +0000

    inherited class 4

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 """A function that returns true or false if the object is an instance of class
-    that inherited from a specified class"""
+    that inherited frm a specified class"""
+
+
 
 
 def inherits_from(obj, a_class):
     """defines if the object is an instance of the class
-        or is inherited from a specified class"""
+        or is inherited frm a specified class"""
     if issubclass (type(obj), a_class):
         if type(obj) == a_class:
             return False
