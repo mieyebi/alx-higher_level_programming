ac20b05ff89214630860d5644cf230be1e8cc039
mieyebi <96272004+mieyebi@users.noreply.github.com>
2022-07-06 21:27:43 +0000

    input/output 5

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" A function that writes an object to a text file using json"""
+
+
+import json
+
+
+def save_to_json(my_obj, filename):
+    """writes a string to a textfile using json"""
+    with open(filename, 'w', encoding="utf-8") as fs:
+        json.dump(my_obj, fs)
