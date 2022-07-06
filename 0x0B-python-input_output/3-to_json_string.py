82f4669fe56959b14fa3bc4063b9921eee892bf3
mieyebi <96272004+mieyebi@users.noreply.github.com>
2022-07-06 21:15:01 +0000

    input/output 3

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""a function that returns the JSON representation of an object"""
+
+
+import json
+
+
+def to_json_string(my job):
+    """a JSON string representation"""
+        a = json.dumps(my_job)
+        return a
