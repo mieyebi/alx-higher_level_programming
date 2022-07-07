53bb2b5b7e02adbb7bfdf2f0312cdbd52a5d307c
mieyebi <96272004+mieyebi@users.noreply.github.com>
2022-07-07 00:08:26 +0000

    input/output 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-"""A script that adds all arguments to a Python list, and then save them to a file"""
+"""A script that adds all arguments to a Python list and then save them to a file"""
 import sys
-
 
 if __name__ == "__main__":
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
