80701302004b428508c772b34ce9a946553bc1f8
mieyebi <96272004+mieyebi@users.noreply.github.com>
2022-07-07 00:04:05 +0000

    input/output 8

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-"""A function that returns the dictionary description with simple data structure
-    for JSON serialization of object"""
+"""A function that returns dictionary description of a json object"""
 
 
 def class_to_json(obj):
