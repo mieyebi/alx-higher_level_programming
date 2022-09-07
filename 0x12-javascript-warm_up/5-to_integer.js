b7009ad24dff45dca5dbdb0e13c9a2cf372532ec
mieyebi <96272004+mieyebi@users.noreply.github.com>
2022-09-07 18:35:31 +0000

    convert to int

---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+// a script that converts arguments to integers
+
+const args = process.argv;
+const a = parseInt(args[2]);
+if (isNaN(a)) {
+  console.log('Not a number');
+} else {
+  console.log(a);
+}
