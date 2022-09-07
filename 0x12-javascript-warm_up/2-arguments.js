92592bf685d91329187448b8780ebc92c5059607
mieyebi <96272004+mieyebi@users.noreply.github.com>
2022-09-07 14:44:15 +0000

    javascript args

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const args = process.argv;
+if (!args[2]) {
+  console.log('No Argument');
+} else {
+  if ((args[2]) && (!args[3])) {
+    console.log('Argument found');
+  } else {
+    console.log('Arguments found');
+  }
+}
