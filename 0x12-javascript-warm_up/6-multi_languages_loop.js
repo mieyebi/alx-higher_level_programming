a8ae37a10a86c336c11d8de81d7baa532f0c6893
mieyebi <96272004+mieyebi@users.noreply.github.com>
2022-09-07 18:53:25 +0000

    multi languages

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,2 +1,9 @@
 #!/usr/bin/node
+// create a multi language loop using while loop
 
+const args = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+let a = 0;
+while (args[a] != null) {
+  console.log(args[a]);
+  a++;
+}
