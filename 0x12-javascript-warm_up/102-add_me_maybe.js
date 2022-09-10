145687b0e9b2c345514cc0006c598c2cba65334a
mieyebi <96272004+mieyebi@users.noreply.github.com>
2022-09-10 07:33:46 +0000

    empty rectangle class

---
+++ 0x12-javascript-warm_up/102-add_me_maybe.js
@@ -0,0 +1,4 @@
+#!/usr/bin/node
+// a function that increments and calls a function
+
+
---
+++ 0x13-javascript_objects_scopes_closures/0-rectangle.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+//An empty rectangle class
+
+module.exports = class Rectangle {
+  constructor () {
+  }
+}
