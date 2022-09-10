b3bcce013e123ef5606bfb1e5a06b5361b49c67d
mieyebi <96272004+mieyebi@users.noreply.github.com>
2022-09-10 10:53:43 +0000

    advanced tasks

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,9 +1,7 @@
 #!/usr/bin/node
 // A script that imports an array and computes a new array
 
-const { list } = require('./100-data');
-const mapList = list.map((value, index) => {
-  return value * index;
-});
-console.log(list);
-console.log(mapList);
+const arr = require('./100-data').list;
+
+console.log(arr);
+console.log(arr.map((x, idx) => x * idx));
