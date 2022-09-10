d71a803ea1ab8c98ac7d282f6aa36e0a53ec3496
mieyebi <96272004+mieyebi@users.noreply.github.com>
2022-09-10 08:39:07 +0000

    define rectangle

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,31 @@
+#!/usr/bin/node
+// A rectangle class that defines the rectangle
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if ((w > 0) && (h > 0)) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let a = 0;
+    while (a < this.height) {
+      console.log('X'.repeat(this.width));
+      a++;
+    }
+  }
+
+  rotate () {
+    let temp;
+    temp = this.width;
+    this.width =  this.height;
+    this.height = temp;
+  }
+
+  double () {
+    this.width = this.width * 2;
+    this.height = this.height * 2;
+  }
+};
