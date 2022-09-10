f5f7573810cc09364c9ec3c3b1aee4c2d3b1c2c0
mieyebi <96272004+mieyebi@users.noreply.github.com>
2022-09-10 10:29:19 +0000

    log me

---
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// A function that prints the number of arguments and the new argument value
+
+let count = 0;
+exports.logMe = function (item) {
+  console.log(`${count}: ${item}`);
+  count++;
+};
