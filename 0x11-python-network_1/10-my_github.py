30930dba0dbca95eacc46cb2e4815300bad4ffaa
mieyebi <96272004+mieyebi@users.noreply.github.com>
2022-09-28 12:47:03 +0000

    my github

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,2 +1,13 @@
 #!/usr/bin/python3
-""" A script that 
+""" A script that takes github credentials and uses the Github api to display id"""
+
+if __name__ == '__main__':
+    import requests
+    from sys import argv
+
+    r = requests.get('https://api.github.com/user',
+                     auth=(argv[1], argv[2]))
+    if r.status_code >= 400:
+        print('None')
+    else:
+        print(r.json().get('id'))
