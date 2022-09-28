ab24cff93236207701d4b0ea2bd44e6d2e0d968f
mieyebi <96272004+mieyebi@users.noreply.github.com>
2022-09-28 11:45:17 +0000

    response header value

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,2 +1,11 @@
 #!/usr/bin/python3
-""" A script that
+""" A script that takes a url, sends a request and displays the value
+    of the variable X-Request-Id in the response header"""
+
+
+if __name__ == '__main__':
+    import requests
+    from sys import argv
+
+    r = requests.get(argv[1])
+    print('r.headers.get(X-Request-Id'))
