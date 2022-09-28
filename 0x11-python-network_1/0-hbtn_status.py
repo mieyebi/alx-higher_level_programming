6082bd28b773763d677da7380a86119457543403
mieyebi <96272004+mieyebi@users.noreply.github.com>
2022-09-28 09:46:53 +0000

    python network

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,17 +1,15 @@
 #!/usr/bin/python3
 """ A script that fetches a url"""
-from urllib.request import urlopen
-
-def hbtn_status_0():
-    """ A function that shows """
-    with urlopen('https://alx_intranet.hbtn.io/status') as response:
-        html = response.read()
-        utf8 = html.decode('utf-8')
-        print("Body response:\n\t- type: {}".format(type(html)))
-        print("\t- content: {}\n\t- utf8 content: {}".
-            format(html, ytf8, end=""))
 
 
 if __name__ == '__main__':
-    hbtn_status_0()
+    from urllib.request import urlopen
 
+
+    with urlopen('https://alx_intranet.hbtn.io/status') as response:
+        html = response.read()
+        utf8 = html.decode('utf-8', "strict")
+        print('Body response:')
+        print('\t- type: {}'.format(type(html)))
+        print('\t- content: {}'.format(html))
+        print('\t- utf8 content: {}'.format(html, utf8, end=""))
